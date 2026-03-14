c78375660cbec8e42503912550de3ddb0c14d27e
Edvinas <edvinasbureika@gmail.com>
2026-03-15 01:13:20 +0200

    single listing route and data

---
+++ controllers/listings/show.php
@@ -0,0 +1,15 @@
+<?php
+
+$config = require basePath('config/db.php');
+$db = new Database($config);
+
+$id = $_GET['id'] ?? null;
+
+$listing = $db->query(
+  "SELECT * FROM listings WHERE id = :id",
+  [
+    'id' => $id
+  ]
+)->fetch();
+
+loadView('listings/show', ['listing' => $listing]);
