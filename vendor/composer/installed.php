f2b927285d634a10a61411f01cb848e4489c7f83
Edvinas Bureika <edvinasbureika@gmail.com>
2026-03-29 07:03:25 +0300

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '0xedvinas/workopia',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7ae1753b53fc8449ab99f95e4868211feb482a4b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '0xedvinas/workopia' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7ae1753b53fc8449ab99f95e4868211feb482a4b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
