8faae183aeb0ca2f583f2fcb58d41e4cc8acb0ca
Unknown <father@falcon.lan>
2026-04-04 21:16:26 +0300

    logout

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -115,4 +115,20 @@ class UserController
 
         redirect('/');
     }
+
+    /**
+     * Logout user and clear session
+     * 
+     * @return void
+     */
+    public function logout()
+    {
+        Session::clearAll();
+
+        $params = session_get_cookie_params();
+
+        setcookie('PHPSESSID', '', time() - 86400, $params['path'], $params['domain']);
+
+        redirect('/');
+    }
 }
--- App/views/partials/navbar.php
+++ App/views/partials/navbar.php
@@ -17,14 +17,13 @@ use Framework\Session;
           <div>
             Welcome, <strong><?= Session::get('user')['name'] ?? '' ?></strong>
           </div>
-          <form action="">
+          <form method="POST" action="/auth/logout">
             <button type="submit" class="text-white inline hover:underline">Logout</button>
           </form>
           <a href="/listings/create"
             class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300"><i class="fa fa-edit"></i> Post a Job</a>
         </div>
       <?php else: ?>
-
         <a href="/auth/login" class="text-white hover:underline">Login</a>
         <a href="/auth/register" class="text-white hover:underline">Register</a>
       <?php endif; ?>
