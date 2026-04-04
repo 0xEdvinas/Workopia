699ce3375e1da931ca5eaf3cdf6938c4a09a0684
Unknown <father@falcon.lan>
2026-04-04 19:43:05 +0300

    user controller and login/register views

--- routes.php
+++ routes.php
@@ -9,3 +9,6 @@ $router->get('/listings/edit/{id}', 'ListingController@edit');
 $router->get('/listings/{id}', 'ListingController@show');
 $router->delete('/listings/{id}', 'ListingController@destroy');
 $router->put('/listings/{id}', 'ListingController@update');
+
+$router->get('/auth/register', 'UserController@create');
+$router->get('/auth/login', 'UserController@login');
